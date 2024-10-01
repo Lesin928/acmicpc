6b192f5a970ddb3587a14df2f40665e2c3959b7d
Lesin <43850042+Lesin928@users.noreply.github.com>
2024-10-02 03:25:50 +0900

    초기화

--- acmicpc/acmicpc/7287.cpp
+++
@@ -1,9 +0,0 @@
-//자신이 백준 온라인 저지(BOJ)에서 맞은 문제의 수와 아이디를 그대로 출력하는 프로그램을 작성하시오.
-//https://www.acmicpc.net/problem/7287
-//point1 : 이 문제는 재체점을 하지 않는다고 한다.
-
-#include <iostream>
-
-int main() {
-    std::cout << "55\nseung1084";
-}
